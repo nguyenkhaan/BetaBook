1428d3cec4d10da681a0dfe7354a8d95c0dc094e
nguyenkhaan <nguyenkhaan2006@gmail.com>
2026-05-29 08:00:50 +0700

    fix: rm betabook folder

--- BetaBook/src/pages/Approval/components/ApprovalHeader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function ApprovalHeader() {
-    return (
-        <div className="flex items-center justify-between">
-            <h1 className="text-gray-900">Quản lý phê duyệt</h1>
-        </div>
-    );
-}
